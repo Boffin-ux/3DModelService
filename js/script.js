bc917a9fe713f3ce880a7e67dfb3fc7791b1f0a3
Boffin-ux <fnamily@mail.ru>
2021-06-17 15:58:57 +0300

    Добавил SmoothScroll Урок №18

--- js/script.js
+++ js/script.js
@@ -113,5 +113,34 @@ window.addEventListener('DOMContentLoaded', () => {
    };
    togglePopUp();
 
+   //Scroll-smooth
+   const scrolled = () => {
 
+      const menuLinks = document.querySelectorAll('menu a:not(.close-btn'),
+         mainLink = document.querySelector('main > a');
+
+      const addScroll = (item) => {
+         const blockID = item.getAttribute('href').substr(1);
+
+         document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         });
+      };
+
+      menuLinks.forEach(elem => {
+         elem.addEventListener('click', (event) => {
+            event.preventDefault();
+            addScroll(elem);
+         });
+      });
+      mainLink.addEventListener('click', (event) => {
+         event.preventDefault();
+         addScroll(mainLink);
+      });
+
+
+   };
+   scrolled();
 });
+
