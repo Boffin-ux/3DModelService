51a1d4fb8a751ed251e21ca10ccd56f80113c886
Boffin-ux <fnamily@mail.ru>
2021-06-17 11:54:20 +0300

    Выполнил основное задание Урок №18

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ window.addEventListener('DOMContentLoaded', () => {
    'use strict';
 
    //Timer
-   function countTimer(deadline) {
+   const countTimer = deadline => {
 
       const timerHours = document.querySelector('#timer-hours'),
          timerMinutes = document.querySelector('#timer-minutes'),
          timerSeconds = document.querySelector('#timer-seconds');
 
-      function getTimeRemaining() {
+      const getTimeRemaining = () => {
          const dateStop = new Date(deadline),
             dateNow = new Date().getTime(),
             timerRemaining = (dateStop - dateNow) / 1000,
@@ -18,15 +18,15 @@ window.addEventListener('DOMContentLoaded', () => {
             hours = Math.floor(timerRemaining / 60 / 60) % 24;
          // day = Math.floor(timerRemaining / 60 / 60 / 24);
          return { timerRemaining, hours, minutes, seconds };
-      }
-      function addZero(num) {
+      };
+      const addZero = num => {
          if (num >= 0 && num <= 9) {
             return `0${num}`;
          } else {
             return num;
          }
-      }
-      function updateClock() {
+      };
+      const updateClock = () => {
          const timer = getTimeRemaining();
          if (timer.timerRemaining > 0) {
 
@@ -39,11 +39,79 @@ window.addEventListener('DOMContentLoaded', () => {
             timerSeconds.textContent = '00';
             clearInterval(idInterval);
          }
-      }
+      };
       const idInterval = setInterval(updateClock, 1000);
       updateClock();
-   }
+   };
 
-   countTimer('17 june 2021');
+   countTimer('30 june 2021');
+
+   //Menu
+   const toggleMenu = () => {
+
+      const btnMenu = document.querySelector('.menu'),
+         menu = document.querySelector('menu'),
+         closeBtn = document.querySelector('.close-btn'),
+         menuItems = menu.querySelectorAll('ul>li');
+
+      const handlerMenu = () => {
+         menu.classList.toggle('active-menu');
+      };
+
+      btnMenu.addEventListener('click', handlerMenu);
+      closeBtn.addEventListener('click', handlerMenu);
+      menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+
+   };
+
+   toggleMenu();
+
+   //popup
+
+   const togglePopUp = () => {
+      const popup = document.querySelector('.popup'),
+         popupBtn = document.querySelectorAll('.popup-btn'),
+         popupClose = document.querySelector('.popup-close'),
+         popupСontent = document.querySelector('.popup-content');
+
+      //animationPopUp
+      let PopUpInterval,
+         animate = false,
+         count = 0;
+
+      const PopUpAnimate = () => {
+         const width = document.documentElement.clientWidth; //получаем ширину страницы
+         PopUpInterval = requestAnimationFrame(PopUpAnimate);
+         count += 1;
+         if (count < 38 && width >= 768) {
+            popupСontent.style.left = `${count}%`;
+         } else {
+            cancelAnimationFrame(PopUpInterval);
+         }
+      };
+      // Отобразить PopUp и запустить анимацию
+      popupBtn.forEach(elem => {
+         elem.addEventListener('click', () => {
+            popup.style.display = 'block';
+            if (!animate) {
+               PopUpInterval = requestAnimationFrame(PopUpAnimate);
+               animate = true;
+            } else {
+               cancelAnimationFrame(PopUpInterval);
+            }
+         });
+      });
+      //Скрыть PopUp и отключить анимацию
+      popupClose.addEventListener('click', () => {
+         popup.style.display = 'none';
+         cancelAnimationFrame(PopUpInterval);
+         animate = false;
+         count = 0;
+      });
+
+
+   };
+   togglePopUp();
+
 
 });
