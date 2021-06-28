ff147e72cb4300ea2cc62f2f80ae0fe4c3cdeac0
Boffin-ux <fnamily@mail.ru>
2021-06-28 17:31:34 +0300

    Выполнил задание Урок №26

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ window.addEventListener('DOMContentLoaded', () => {
       const idInterval = setInterval(updateClock, 1000);
       updateClock();
    };
-   countTimer('30 june 2021');
+   countTimer('11 july 2021');
 
    //Menu
    const toggleMenu = () => {
@@ -50,7 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
       const menu = document.querySelector('menu');
 
       document.body.addEventListener('click', event => {
-         event.preventDefault();
+         //event.preventDefault();
          const target = event.target;
          if (target.closest('.menu')) {
             menu.classList.add('active-menu');
@@ -314,12 +314,14 @@ window.addEventListener('DOMContentLoaded', () => {
          const checkInput = (event, closeTarget) => {
             const target = event.target;
             if (target.value) {
-               if (target.closest(`#form${closeTarget}-name`) || target.closest(`#form${closeTarget}-message`)) {
-                  target.value = target.value.replace(/[^а-яё -]/ig, '');
+               if (target.closest(`#form${closeTarget}-name`)) {
+                  target.value = target.value.replace(/[^а-яё\s]/ig, '');
                } else if (target.closest(`#form${closeTarget}-email`)) {
                   target.value = target.value.replace(/[^a-z@_.!~*'-]/ig, '');
                } else if (target.closest(`#form${closeTarget}-phone`)) {
-                  target.value = target.value.replace(/[^0-9()-]/g, '');
+                  target.value = target.value.replace(/[^0-9+]/g, '');
+               } else if (target.closest(`#form${closeTarget}-message`)) {
+                  target.value = target.value.replace(/[^а-яё\s0-9.?!,:;()""-]/g, '');
                }
             } else {
                return;
@@ -415,4 +417,83 @@ window.addEventListener('DOMContentLoaded', () => {
       });
    };
    calc(100);
+
+   //send-ajax-form
+   const sendForm = () => {
+      const errorMessage = 'Что-то пошло не так...',
+         preload = `
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+      <rect x="19" y="29.5" width="12" height="41" fill="#40b5fd">
+         <animate attributeName="y" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" 
+         values="17.199999999999996;29.5;29.5" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.2s"></animate>
+         <animate attributeName="height" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" 
+         values="65.60000000000001;41;41" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.2s"></animate>
+      </rect>
+      <rect x="44" y="29.5" width="12" height="41" fill="#74c4f5">
+         <animate attributeName="y" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" 
+         values="20.274999999999995;29.5;29.5" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.1s"></animate>
+         <animate attributeName="height" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" 
+         values="59.45000000000001;41;41" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.1s"></animate>
+      </rect>
+      <rect x="69" y="29.5" width="12" height="41" fill="#9dd5f7">
+         <animate attributeName="y" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" 
+         values="20.274999999999995;29.5;29.5" keySplines="0 0.5 0.5 1;0 0.5 0.5 1"></animate>
+         <animate attributeName="height" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" 
+         values="59.45000000000001;41;41" keySplines="0 0.5 0.5 1;0 0.5 0.5 1"></animate>
+      </rect>
+      `,
+         successMessage = 'Спасибо! Мы скоро с вами свяжемся...';
+
+      const form = document.getElementById('form1'),
+         formFooter = document.getElementById('form2'),
+         formPopup = document.getElementById('form3');
+
+
+
+      const statusMessage = document.createElement('div');
+      statusMessage.style.cssText = 'font-size: 2rem;';
+      statusMessage.style.color = '#fff';
+
+      const postData = (body, outputData, errorData) => {
+         const request = new XMLHttpRequest();
+         request.addEventListener('readystatechange', () => {
+            if (request.readyState !== 4) {
+               return;
+            }
+            if (request.status === 200) {
+               outputData();
+            } else {
+               errorData(request.status);
+            }
+         });
+         request.open('POST', './server.php');
+         request.setRequestHeader('Content-Type', 'application/json');
+         request.send(JSON.stringify(body));
+      };
+      const addMessageForm = (event, form) => {
+         event.preventDefault();
+         const inputs = form.querySelectorAll('input');
+         form.appendChild(statusMessage);
+         statusMessage.innerHTML = preload;
+         const formData = new FormData(form),
+            body = {};
+
+         formData.forEach((val, key) => {
+            body[key] = val;
+         });
+         postData(body, () => {
+            statusMessage.textContent = successMessage;
+            inputs.forEach(item => item.value = '');
+         }, error => {
+            statusMessage.textContent = errorMessage;
+            inputs.forEach(item => item.value = '');
+            console.error(error);
+         });
+      };
+      form.addEventListener('submit', event => addMessageForm(event, form));
+      formFooter.addEventListener('submit', event => addMessageForm(event, formFooter));
+      formPopup.addEventListener('submit', event => addMessageForm(event, formPopup));
+   };
+   sendForm();
+
 });
